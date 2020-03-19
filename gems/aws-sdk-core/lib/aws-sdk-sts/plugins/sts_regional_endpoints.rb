63c0095f4047b8d92da9e7d48d062683cd9ebd74
Alex Woods <alextwoods@outlook.com>
2020-03-19 12:30:38 -0700

    Another change to core without changelog

--- gems/aws-sdk-core/lib/aws-sdk-sts/plugins/sts_regional_endpoints.rb
+++ gems/aws-sdk-core/lib/aws-sdk-sts/plugins/sts_regional_endpoints.rb
@@ -9,7 +9,7 @@ module Aws
           doc_type: String,
           docstring: <<-DOCS) do |cfg|
 Passing in 'regional' to enable regional endpoint for STS for all supported
-regions (except 'aws-global'), defaults to 'legacy' mode, using global endpoint
+regions (except 'aws-global'), defaults to 'regional' mode, using global endpoint
 for legacy regions.  
           DOCS
           resolve_sts_regional_endpoints(cfg)
